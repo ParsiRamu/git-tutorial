c53df6fabdc4839811238cf8194ef57cdd544cec
Lakshman <Lakshman26spark@gmail.com>
2026-07-23 01:02:23 +0530

    remove today print statement in sample.py

--- sample.py
+++ sample.py
@@ -3,5 +3,4 @@ print("hello world !")
 print("hai")
 
 print("laxman added one change")
-print("i dont know the remote changes")
-print("today was thursday")
+print("i dont know the remote changes") 
